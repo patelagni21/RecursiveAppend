6aed7bc1692fc3ce9e6255b1ddfc8a60c6639695
Agni Patel <77357349+patelagni21@users.noreply.github.com>
2021-02-25 10:59:38 -0500

    Add files via upload

---
+++ recursiveappend.java
@@ -0,0 +1,17 @@
+public class RecursiveAppend {
+
+// Returns the original string appended to the original string n times
+public static String appendNTimes (String original, int n) {
+
+if (n==0){
+return original;
+}
+return original + appendNTimes(original, n-1);
+}
+
+public static void main (String[] args) {
+    String string = args[0];
+        int times = Integer.parseInt(args[1]);
+              System.out.println(appendNTimes(string, times));
+}
+}
